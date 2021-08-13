c4a02f24de47d19aa6fbd57e895876abf3beebba
Gimba <martin.rosellen@fu-berlin.de>
2021-08-13 09:05:40 +0200

    corrected help comments

--- KBL.py
+++ KBL.py
@@ -33,17 +33,19 @@ import time
 
 def main(args):
     tic = time.time()
-    parser = argparse.ArgumentParser(description=r'Calculate the KBL value for the angle files (.xvg format) found in '
-                                                 'the two specified directories.\n\n Files that get generated: \n\n{'
-                                                 'output_filename} - .pml that can be loaded in pml (@{'
-                                                 'output_filename} and colors residues with low kbl-value white and '
-                                                 'high kbl-values blue\n'
-                                                 'hist.{output_filename}.png - image of the generated histogram\n'
-                                                 'hist.{outp_filemane}.dat - data of the histogram',
-                                     formatter_class=RawTextHelpFormatter)
+    parser = argparse.ArgumentParser(
+        description='Calculate the Jensen Shannon divergence between angle distributions extracted from specified '
+                    'files\n\n'
+                    'Files that get generated \n\n{'
+                    'output_filename} - .pml that can be loaded in pml (@{'
+                    'output_filename} and colors residues with low kbl-value white and '
+                    'high kbl-values blue\n'
+                    'hist.{output_filename}.png - image of the generated histogram\n'
+                    'hist.{outp_filemane}.dat - data of the histogram',
+        formatter_class=RawTextHelpFormatter)
     # TODO: give option to read in whole directories and specify how many frames should be considered
-    parser.add_argument('files1', help='reference directory, e.g. ./WT/gromacs_psi_phi_chi/')
-    parser.add_argument('files2', help='to compare to directory, e.g. ./R2215A/gromacs_psi_phi_chi/')
+    parser.add_argument('files1', help='list of trajectory files or directory')
+    parser.add_argument('files2', help='list of trajectory files or directory')
     parser.add_argument('-m', dest='mutations', nargs='?', help='mutations introduced to the structure of the second ' \
                                                                 'directory. e.g. ARG2215,ALA2215', default="")
     parser.add_argument('-o', dest='output_filename', nargs='?', help='name of .pml file that gets written ('
@@ -142,7 +144,6 @@ def main(args):
     jsd = k.get_jsd(distributions)
 
     # generate histogram
-
 
     if args.show:
         # TODO: give option to plot only a range of residues (the 10 highest JSD values)
